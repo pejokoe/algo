8ec486d03a58eb43d106e4f8b2fc51423ba17567
Peter <1822913@stud.hs-mannheim.de>
2022-12-01 09:08:22 +0100

    Stack main program

---
+++ Stack/src/Program_Stack.java
@@ -0,0 +1,15 @@
+
+public class Program_Stack {
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+			for (int i = 0; i < 10; i++) {
+				stack.push(i);
+			}
+			int a = (int) stack.pop();
+			System.out.println(a);
+			int limit = stack.size();
+			for (int i = 0; i < limit; i++) {
+				System.out.println(stack.pop());
+			}
+	}
+}
